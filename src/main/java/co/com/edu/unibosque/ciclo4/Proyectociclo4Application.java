0ff6daa195cf1f3b54632d72efab5bb48ee77a5a
Edwin Castro <edwincas_17@hotmail.com>
2021-11-13 11:52:47 -0500

    Primer Commit #1
    Configuración Inicial

---
+++ src/main/java/co/com/edu/unibosque/ciclo4/Proyectociclo4Application.java
@@ -0,0 +1,13 @@
+package co.com.edu.unibosque.ciclo4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyectociclo4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyectociclo4Application.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/edu/unibosque/ciclo4/Proyectociclo4ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.edu.unibosque.ciclo4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyectociclo4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
